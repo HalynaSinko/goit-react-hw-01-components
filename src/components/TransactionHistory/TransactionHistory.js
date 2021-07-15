b8285815e1561b944f769c3c5c0e82f30efc8dbf
Halyna Sinko <galinasinko14@gmail.com>
2021-07-15 21:03:35 +0300

    fix import

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -1,8 +1,6 @@
-import React from "react";
 import PropTypes from "prop-types";
 
 import TransactionTr from "./TransactionTr";
-
 import s from "./TransactionHistory.module.css";
 
 const TransactionHistory = ({ items }) => {
